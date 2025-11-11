714a98c013f0c2226015440b4fcd87abc0670f76
kishorbha <kishorbhandari608@gmail.com>
2025-11-11 08:47:58 +0545

    Clean up: Remove .git.bak directories and update .gitignore

--- contest-platform/src/enrollments/dto/enroll-contest.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class EnrollContestDto {
-  @ApiProperty({ example: 'contest-id' })
-  @IsString()
-  contestId: string;
-}
-
--- contest-platform/src/payments/dto/confirm-payment.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class ConfirmPaymentDto {
-  @ApiProperty({ example: 'transaction-id-123' })
-  @IsString()
-  transactionId: string;
-}
-
